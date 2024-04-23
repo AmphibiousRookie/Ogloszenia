9082395b8e1d3fc722116fa7bfae428c9e0569c5
AmphibiousRookie <157484275+AmphibiousRookie@users.noreply.github.com>
2024-04-23 10:55:20 +0200

    Delivery has arrived >:)

---
+++ Ogłoszenia.js
@@ -0,0 +1,39 @@
+const uzytkownik1 = {
+    imie: '1. Daniel Craig. biografia',
+    desc: 'Biografia Daniela Craiga, niedrogo sprzedam',
+    telefon: '601601601',
+}
+
+const uzytkownik2 = {
+    imie: '2. Selekcja',
+    desc: 'Sprzedam "Selekcja" J. Kellermana, niezniszczona',
+    telefon: '601601601',
+}
+
+const uzytkownik3 = {
+    imie: '3. Buick',
+    desc: 'Sprzedam horror Stephena Kinga w dobrym stanie',
+    telefon: '608608608',
+}
+
+const uzytkownik4 = {
+    imie: '4. Tytus, Romek i Atomek',
+    desc: 'Ks. IV do sprzedania, stan dobry',
+    telefon: '608608608',
+}
+
+document.getElementById("tekst1").innerHTML = uzytkownik1.imie;
+document.getElementById("tekst2").innerHTML = uzytkownik1.desc;
+document.getElementById("tekst3").innerHTML = uzytkownik1.telefon;
+
+document.getElementById("tekst4").innerHTML = uzytkownik2.imie;
+document.getElementById("tekst5").innerHTML = uzytkownik2.desc;
+document.getElementById("tekst6").innerHTML = uzytkownik2.telefon;
+
+document.getElementById("tekst7").innerHTML = uzytkownik3.imie;
+document.getElementById("tekst8").innerHTML = uzytkownik3.desc;
+document.getElementById("tekst9").innerHTML = uzytkownik3.telefon;
+
+document.getElementById("tekst10").innerHTML = uzytkownik4.imie;
+document.getElementById("tekst11").innerHTML = uzytkownik4.desc;
+document.getElementById("tekst12").innerHTML = uzytkownik4.telefon;
